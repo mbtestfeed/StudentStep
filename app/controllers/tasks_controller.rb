b5afe75b8a3f94dc699854ca4dbb9255f0bf892e
mbtestfeed <michael.s.ball@gmail.com>
2017-05-24 10:51:33 +1200

    Tasks index now only show tasks for parent plan

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.where(plan: params[:plan_id])
   end
 
   # GET /tasks/1
