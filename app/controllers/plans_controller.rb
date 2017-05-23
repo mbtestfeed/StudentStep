793c724f4c97d7dd0f3945e92976da45a1e81933
mbtestfeed <michael.s.ball@gmail.com>
2017-05-23 23:25:21 +1200

    Small edits on wrong project

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -28,7 +28,7 @@ class PlansController < ApplicationController
 
     respond_to do |format|
       if @plan.save
-        format.html { redirect_to @plan, notice: 'Plan was successfully created.' }
+        format.html { redirect_to plan_tasks_path(@plan), notice: 'Plan was successfully created.' }
         format.json { render :show, status: :created, location: @plan }
       else
         format.html { render :new }
