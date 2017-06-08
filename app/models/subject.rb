8190d5b6d792f3525ed78edf6fdb23e4aa632b31
mbtestfeed <michael.s.ball@gmail.com>
2017-06-08 15:38:59 +1200

    Add plan to Subject model. Display plans on subject show.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,6 +3,7 @@ class Subject < ApplicationRecord
 
   has_many :sub_stu_links
   has_many :students, through: :sub_stu_links
+  has_many :plans
 
   validates_uniqueness_of :name, scope: :teacher, message: 'is taken for this teacher.'
 end
