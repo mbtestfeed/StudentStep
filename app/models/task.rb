8df8cb2eb55be5793af97b0dba438c592ea44f7d
mbtestfeed <michael.s.ball@gmail.com>
2017-06-20 00:09:37 +1200

    Add plans/_entry partial form, format as appropriate.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,11 @@
 class Task < ApplicationRecord
+  TASKS = ['Description', 'Single-line input', 'Multi-line input'] #TODO: Change these to symbols. Track down all usages and refactor
+
   belongs_to :plan
   belongs_to :parent_task, class_name: 'Task', foreign_key: :parent_task_id, optional: true
 
   has_many :tasks
   has_many :entries
 
-  validates :task_type, inclusion: {}
+  validates :task_type, inclusion: {in: TASKS}
 end
